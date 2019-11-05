6ce172cc8b52d82645bf53d60d53160706ad4041
ayoitunu <ayoitunu@yahoo.com>
2019-11-05 10:28:17 +0100

    Updated

--- myblog_project/myblog_app/views.py
+++ myblog_project/myblog_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
+# the . means the current directly or app we re  currently in
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'myblog_app/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'myblog_app/post_list.html', {'posts': posts})
 
 
